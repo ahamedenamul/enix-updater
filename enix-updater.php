a0a3f20606d634835b2fe51bea7dff8bfd6f7a26
Ahamed Enamul <freelance.enamul@gmail.com>
2026-05-14 09:48:28 +0600

    Initial plugin commit

---
+++ enix-updater.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Enix Updater
+ * Plugin URI:  https://example.com/
+ * Description: A basic plugin for Enix Updater.
+ * Version:     1.0.0
+ * Author:      Enix
+ * Author URI:  https://example.com/
+ * License:     GPL-2.0+
+ * Text Domain: enix-updater
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin does nothing.
+ * You can add your basic functions here.
+ */
